99d96910a5988a4aa87a70cf720fa4e12697beb4
Victor Wang <wjp2013@gmail.com>
2014-08-12 17:01:20 +0800

    修改 gemfile 的引入方式

---
+++ lib/igetui_ruby.rb
@@ -0,0 +1,34 @@
+require 'forwardable'
+require 'net/http'
+require 'uri'
+require 'json'
+require 'digest/md5'
+require 'base64'
+
+module IGeTui
+  extend self
+  extend Forwardable
+
+  API_URL = "http://sdk.open.api.igexin.com/apiex.htm"
+
+  attr_reader :pusher
+
+  def_delegators :pusher, :push_message_to_single
+  def_delegators :pusher, :push_message_to_list
+  def_delegators :pusher, :push_message_to_app
+  def_delegators :pusher, :stop
+  def_delegators :pusher, :get_client_id_status
+  def_delegators :pusher, :get_content_id, :cancel_content_id
+
+  def pusher(app_id, api_key, master_secret)
+    @pusher ||= IGeTui::Pusher.new(API_URL, app_id, api_key, master_secret)
+  end
+
+end
+
+require "igetui/version"
+require 'protobuf/GtReq.pb'
+require "igetui/template"
+require "igetui/message"
+require "igetui/pusher"
+require "igetui/client"
